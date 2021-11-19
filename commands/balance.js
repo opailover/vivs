e547f6f9c3b701b73ad4924c8cc3ebd4953b8508
opailover <94675587+opailover@users.noreply.github.com>
2021-11-19 13:38:41 +0530

    Create balance.js

---
+++ commands/balance.js
@@ -0,0 +1,51 @@
+const config = require('config');
+module.exports = {
+    name: "Balance",
+    usage: "balance [user]",
+    accessLevel: 0,
+    commands: ["balance", "bal"],
+    run: function run(user, userID, channel, message, args, event, bot){
+        var target = args[1] ? args[1].replace(/[!<>@]/g, "") : userID;
+
+        var server = bot.channels[channel].guild_id;
+        bot.database.getBalance(target, server)
+            .then(function(result){
+                if(!result[0]){
+                    bot.log("User doesn't have a thing");
+
+                    if(bot.users[target]){
+
+                        bot.database.createServerUser(server, target, bot.users[target].username+"#"+bot.users[target].discriminator)
+                            .then(function(){
+                                return bot.getCurrencyFor(server, balance);
+                            })
+                            .then(function(currency){
+                                bot.sendMessage({
+                                    to: channel,
+                                    message: `:dollar: <@${target}> has **0** ${currency}...`
+                                });
+                            })
+                            .catch(function(err){
+                               bot.sendMessage({
+                                   to: channel,
+                                   message: `:bangbang: Something has gone horribly, horribly wrong. Let Big P#1843 know about this:\n\`\`\`\n${err}\n\`\`\``
+                               });
+                            });
+                    }else{
+                        bot.sendMessage({
+                            to: channel,
+                            message: ":bangbang: That user doesn't exist. Try @mentioning them instead"
+                        });
+                    }
+                }else{
+                    var balance = result[0].balance;
+                    bot.getCurrencyFor(server, balance).then(function(currency){
+                        bot.sendMessage({
+                            to: channel,
+                            message: `:dollar: <@${target}> has **${bot.numberWithCommas(balance)}** ${currency}.`
+                        });
+                    });
+                }
+            });
+    }
+};
